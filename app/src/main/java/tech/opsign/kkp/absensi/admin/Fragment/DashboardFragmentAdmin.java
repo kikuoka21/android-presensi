37922a36490b03dc1b931a6118115884068fc32e
Thendy Suteja <kikuoka21@gmail.com>
2019-04-16 14:59:16 +0700

    laptop3

--- app/src/main/java/tech/opsign/kkp/absensi/admin/Fragment/DashboardFragmentAdmin.java
+++ app/src/main/java/tech/opsign/kkp/absensi/admin/Fragment/DashboardFragmentAdmin.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +17,9 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import Tools.GenKey;
 import Tools.Utilities;
@@ -23,29 +30,58 @@ public class DashboardFragmentAdmin extends Fragment {
     private GenKey key;
     private View v;
     private SharedPreferences sp;
-    private JSONObject jray;
+
+
+
+    private RecyclerView recyclerView;
+    private List<Model> modelList = new ArrayList<>();
+    private Adapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getActivity().setTitle(R.string.home);
         v = inflater.inflate(R.layout.fragmenadmin, container, false);
-        try {
-            jray = new JSONObject("{\"data\":[{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"}]}");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
         key = new GenKey();
         sp = v.getContext().getSharedPreferences(key.key(9145), 0x0000);
-        getnama();
+//        getnama();
+        Log.e("ER", "kandnwkaksdad");
+        adapter = new Adapter(modelList);
+        recyclerView = (RecyclerView) v.findViewById(R.id.list_kehadiran_siswa);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(v.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setNestedScrollingEnabled(false);
 
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+        getnama();
         return v;
     }
 
     private void getnama() {
         try {
 //            JSONObject obj = new JSONObject(sp.getString(GenKey.GenKey(1102), ""));/**/
-            ((TextView) v.findViewById(R.id.textView14)).setText(sp.getString(key.key(1102), ""));
+//            ((TextView) v.findViewById(R.id.textView14)).setText(sp.getString(key.key(1102), ""));
+            JSONObject jray = new JSONObject("{\"data\":[{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"},{\"nis\":\"130005\",\"nama\":\"Gunawan Pria Utama, S.Kom., M.Kom\",\"kelas\":\"11 IPA-1\",\"alasan\":\"Tidak Ada Alasan\"}]}");
+            Model row;
+            JSONArray aray = jray.getJSONArray("data");
+            if (aray != null && aray.length() > 0) {
+               for(int i =0; i<aray.length();i++){
+                   jray = aray.getJSONObject(i);
+                   row = new Model(
+                           jray.getString("nis"),
+                           jray.getString("nama"),
+                           jray.getString("kelas"),
+                           jray.getString("alasan")
+
+                   );
+                   modelList.add(row);
+               }
+            }
+            adapter.notifyDataSetChanged();
+
         } catch (Exception e) {
             ((TextView) v.findViewById(R.id.textView14)).setText("-");
             Utilities.codeerror(getContext(), "ER0011");
