5ef3d9d447a52a6112fda4f8632c637a0b796d44
Thendy Suteja <kikuoka21@gmail.com>
2019-06-27 16:15:54 +0700

    blajar md5 di splashscreen.java

--- app/src/main/java/tech/opsign/kkp/absensi/SplashScreen.java
+++ app/src/main/java/tech/opsign/kkp/absensi/SplashScreen.java
@@ -26,7 +26,12 @@ import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.MessageDigestSpi;
+import java.security.Provider;
+import java.security.Security;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import Tools.GenKey;
@@ -71,8 +76,165 @@ public class SplashScreen extends AppCompatActivity {
 
         }
 
-        loadIMEI();
+//        loadIMEI();\
+        md5a("aa");
+//        check();
+
     }
+    private void check(){
+        for(int i=0;i<255; i++ ){
+            Log.e("ER__"+i, Integer.toHexString(i));
+        }
+    }
+    private void md5a(String s) {
+        String MD5 = "MD5";
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance(MD5);
+            digest.update(s.getBytes());
+
+
+
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuilder hexString = new StringBuilder();
+
+            Log.e("MD5__b_", Arrays.toString(s.getBytes()));
+            Log.e("MD5__b_", String.valueOf(digest.digest()));
+            Log.e("MD5__b_", String.valueOf(messageDigest));
+            Log.e("MD5__b_", Arrays.toString(digest.digest()));
+            Log.e("MD5__b_", Arrays.toString(messageDigest));
+            int i = 0;
+            for (byte aMessageDigest : messageDigest) {
+
+                String h = Integer.toHexString(0xFF & aMessageDigest);
+                while (h.length() < 2)
+                    h = "0" + h;
+//                Log.e("MD5_ messageDigest ", String.valueOf(messageDigest[i]));
+//                Log.e("MD5_ aMessageDigest", String.valueOf( aMessageDigest));
+//                Log.e("MD5_ 0xFF & aMess", String.valueOf( 0xFF & aMessageDigest));
+//                Log.e("MD5_ 0xFF", String.valueOf( 0xFF));
+//                Log.e("MD5___h", h);
+                hexString.append(h);
+                i++;
+            }
+            Log.e("ER", String.valueOf(hexString));
+            Log.e("ER", hexString.toString());
+
+            Log.e("ER", toHexString(s.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private static final int   INIT_A     = 0x67452301;
+    private static final int   INIT_B     = (int) 0xEFCDAB89L;
+    private static final int   INIT_C     = (int) 0x98BADCFEL;
+    private static final int   INIT_D     = 0x10325476;
+    private static final int[] SHIFT_AMTS = { 7, 12, 17, 22, 5, 9, 14, 20, 4,
+            11, 16, 23, 6, 10, 15, 21    };
+    private static final int[] TABLE_T    = new int[64];
+    static
+    {
+        for (int i = 0; i < 64; i++)
+            TABLE_T[i] = (int) (long) ((1L << 32) * Math.abs(Math.sin(i + 1)));
+    }
+
+    public static byte[] computeMD5(byte[] message)
+    {
+        int messageLenBytes = message.length;
+        int numBlocks = ((messageLenBytes + 8) >>> 6) + 1;
+        int totalLen = numBlocks << 6;
+        byte[] paddingBytes = new byte[totalLen - messageLenBytes];
+        paddingBytes[0] = (byte) 0x80;
+        long messageLenBits = (long) messageLenBytes << 3;
+        for (int i = 0; i < 8; i++)
+        {
+            paddingBytes[paddingBytes.length - 8 + i] = (byte) messageLenBits;
+            messageLenBits >>>= 8;
+        }
+        int a = INIT_A;
+        int b = INIT_B;
+        int c = INIT_C;
+        int d = INIT_D;
+        int[] buffer = new int[16];
+        for (int i = 0; i < numBlocks; i++)
+        {
+            int index = i << 6;
+            for (int j = 0; j < 64; j++, index++)
+                buffer[j >>> 2] = ((int) ((index < messageLenBytes) ? message[index]
+                        : paddingBytes[index - messageLenBytes]) << 24)
+                        | (buffer[j >>> 2] >>> 8);
+            int originalA = a;
+            int originalB = b;
+            int originalC = c;
+            int originalD = d;
+            for (int j = 0; j < 64; j++)
+            {
+                int div16 = j >>> 4;
+                int f = 0;
+                int bufferIndex = j;
+                switch (div16)
+                {
+                    case 0:
+                        f = (b & c) | (~b & d);
+                        break;
+                    case 1:
+                        f = (b & d) | (c & ~d);
+                        bufferIndex = (bufferIndex * 5 + 1) & 0x0F;
+                        break;
+                    case 2:
+                        f = b ^ c ^ d;
+                        bufferIndex = (bufferIndex * 3 + 5) & 0x0F;
+                        break;
+                    case 3:
+                        f = c ^ (b | ~d);
+                        bufferIndex = (bufferIndex * 7) & 0x0F;
+                        break;
+                }
+                int temp = b
+                        + Integer.rotateLeft(a + f + buffer[bufferIndex]
+                                + TABLE_T[j],
+                        SHIFT_AMTS[(div16 << 2) | (j & 3)]);
+                a = d;
+                d = c;
+                c = b;
+                b = temp;
+            }
+            a += originalA;
+            b += originalB;
+            c += originalC;
+            d += originalD;
+        }
+        byte[] md5 = new byte[16];
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            int n = (i == 0) ? a : ((i == 1) ? b : ((i == 2) ? c : d));
+            for (int j = 0; j < 4; j++)
+            {
+                md5[count++] = (byte) n;
+                n >>>= 8;
+            }
+        }
+        return md5;
+    }
+
+    public static String toHexString(byte[] b)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < b.length; i++)
+        {
+            sb.append(String.format("%02X", b[i] & 0xFF));
+        }
+        return sb.toString();
+    }
+
+
 
     private void loadIMEI() {
 
