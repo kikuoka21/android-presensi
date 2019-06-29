ec21b7fb2dc8a0b38e5bf50a6905994c133f4a81
Thendy Suteja <kikuoka21@gmail.com>
2019-06-29 21:25:38 +0700

    ubahan

--- app/src/main/java/Tools/GenKey.java
+++ app/src/main/java/Tools/GenKey.java
@@ -8,6 +8,7 @@ public class GenKey {
         web = "192.168.0.17";
 //        web = "192.168.0.18";
 //        web = "192.168.12.17";
+        web = "192.168.43.242";
         port = ":8000";
         head = "http://" + web + port;
         switch (str) {
